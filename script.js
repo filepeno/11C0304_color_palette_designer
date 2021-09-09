f01cad9259bcb1f3200647e090612fac5e57017f
Filip <fili4252@stud.kea.dk>
2021-09-09 12:25:29 +0200

    created basic html structure, some styling, made js track changes of colour and harmony selector

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 "use strict";
+
+const HTML = {};
+
+document.addEventListener("DOMContentLoaded", start);
+
+function start() {
+  console.log("start()");
+  HTML.colorPicker = document.querySelector("input");
+  HTML.harmonySelector = document.querySelectorAll("option");
+  HTML.harmonySelected = document.querySelector("select");
+
+  HTML.colorDisplay = document.querySelector(".colorShow");
+  trackColorPicker();
+  trackHarmonySelector();
+}
+
+function trackColorPicker() {
+  console.log("trackColorPicker()");
+  HTML.colorPicker.addEventListener("input", updateColors);
+}
+
+function trackHarmonySelector() {
+  console.log("trackHarmonySelector()");
+  HTML.harmonySelector.forEach((element) => {
+    element.addEventListener("click", updateColors);
+  });
+}
+
+//DELEGATOR
+function updateColors() {
+  console.log(HTML.harmonySelected.value);
+  const colorHex = HTML.colorPicker.value;
+  console.log(colorHex);
+  displayColor(colorHex);
+}
+
+function displayColor(valueHex) {
+  HTML.colorDisplay.style.background = valueHex;
+}
